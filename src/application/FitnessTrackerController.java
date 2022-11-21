01afe4c9738745f0a4478a1fe8dcdbce25e55fb1
Christian Salinas <christian.salinas@ucalgary.ca>
2022-11-21 15:14:20 -0700

    Starting the changing the scene for exercise. Still trying to figure out how to link it to the Exercise class.

--- src/application/FitnessTrackerController.java
+++ src/application/FitnessTrackerController.java
@@ -2,11 +2,16 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class FitnessTrackerController {
-
+	Stage applicationStage;
+	
     @FXML
     private TextField weightGoalTextfield;
     
@@ -15,6 +20,7 @@ public class FitnessTrackerController {
 
     @FXML
     void weightGoalPlan(ActionEvent event) {
+    	
     	double weightValue = Double.parseDouble(weightGoalTextfield.getText());
     	errorMessage.setText("");
     	
@@ -23,20 +29,30 @@ public class FitnessTrackerController {
     	} else {
     		Goals userGoal = new Goals(weightValue);
     	}
-    	
-    	
     }
-
+	
+    @FXML
+	void excercisePlan(ActionEvent event) {
+    	
+    	HBox excerciseContainer = new HBox();
+    	Label excerciseLabel = new Label("How many days to do wanna workout?");
+    	ChoiceBox<Integer> excerciseChoiceBox = new ChoiceBox<Integer>();
+    	for(int days = 0; days <= 7; days++) {
+    		excerciseChoiceBox.getItems().add(days);
+    	}
+    	
+    	excerciseContainer.getChildren().addAll(excerciseLabel, excerciseChoiceBox);
+    	Scene excerciseScene = new Scene(excerciseContainer);
+    	applicationStage.setScene(excerciseScene);
+    	
+	}
     @FXML
     void nutritionPlan(ActionEvent event) {
     	//System.out.println("nutritionPlan was clicked");
 
     }
 
-    @FXML
-    void excercisePlan(ActionEvent event) {
-    	//System.out.println("excercisePlan was clicked");
-    }
+    
 
 }
 
