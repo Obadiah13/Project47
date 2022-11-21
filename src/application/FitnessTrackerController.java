8510876072c16be62d74dbe3946993b130c69ed9
Christian Salinas <christian.salinas@ucalgary.ca>
2022-11-21 11:08:23 -0700

    Added variables for the methods.

--- src/application/FitnessTrackerController.java
+++ src/application/FitnessTrackerController.java
@@ -1,7 +1,29 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class FitnessTrackerController {
 
+    @FXML
+    private TextField weightGoalTextfield;
+
+    @FXML
+    void weightGoalPlan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nutritionPlan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void excercisePlan(ActionEvent event) {
+
+    }
+
 }
+
 
