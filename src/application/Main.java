3eb771c3e35241c2bee5c616b86a13bf839912d7
Christian Salinas <christian.salinas@ucalgary.ca>
2022-11-21 10:27:44 -0700

    Added some labels, textfields, and buttons. Basic functionality.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,23 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			
 			FXMLLoader loader = new FXMLLoader();
-			Scene scene = new Scene(root,400,400);
+			VBox root = loader.load(new FileInputStream("src/application/FitnessTracker.fxml"));
+			Scene scene = new Scene(root,300,300);
 			
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Fitness Tracker");
