581e51df35d3dfee05ee10246fb39ef5928a45ec
Christian Salinas <christian.salinas@ucalgary.ca>
2022-11-23 19:16:16 -0700

    Added a progress scene, so the user knows their fitness progress.

--- src/application/FitnessTrackerController.java
+++ src/application/FitnessTrackerController.java
@@ -3,10 +3,12 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class FitnessTrackerController {
@@ -46,10 +48,36 @@ public class FitnessTrackerController {
     	applicationStage.setScene(excerciseScene);
     	
 	}
+    
     @FXML
     void nutritionPlan(ActionEvent event) {
     	//System.out.println("nutritionPlan was clicked");
 
+    }
+    
+    @FXML
+    void checkProgress(ActionEvent event) {
+    	VBox checkProgress = new VBox();
+    	
+    	for(int rows = 0; rows < 2; rows++) {
+	    	HBox checkProgessExcercise = new HBox();
+	    	Label excercise = new Label("Exercise: ");
+	    	HBox checkProgessNutrition = new HBox();
+	    	Label nutrition = new Label("Nutrition: ");
+	    	
+	    	checkProgessExcercise.getChildren().addAll(excercise);	
+	    	checkProgessNutrition.getChildren().addAll(nutrition);
+	    	
+	    	checkProgress.getChildren().add(checkProgessExcercise);
+	    	checkProgress.getChildren().add(checkProgessNutrition);
+    	}
+    	
+    	Button done = new Button("Done");
+    	
+    	
+    	Scene checkProgressScene = new Scene(checkProgress);
+    	applicationStage.setScene(checkProgressScene);
+    	
     }
 
     
