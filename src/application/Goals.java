1094543d3b6320c10d7705d76539cbf86f1d379d
Christian Salinas <christian.salinas@ucalgary.ca>
2022-12-04 17:13:32 -0700

    Since Ammar couldn't push and commit his work, I got the files to push and commit instead. Main changes involve myPlan.fxml, myPlanController.java to switch the scenes to output the user's health plan.

--- src/application/Goals.java
+++ src/application/Goals.java
@@ -1,18 +1,69 @@
 package application;
 
-public class Goals {
+import javafx.scene.chart.XYChart;
+
+public class Goals extends FitnessTrackerController {
 //Setting goals that the user has, etc, goes into this class
-	private String goalWeight;
+	private String goalWeightLoss;
+	private double goalWeight;
 	private double currentWeight;
+	private int weightDifference;
+	private double calories = 0.0;
+	private static final Goals instance = new Goals();
 	
+	// Getters and Setters
+	public static Goals getInstance() {
+		return instance;
+	}
+
+	public void setCalories(double calories) {
+		this.calories = calories;
+	}
+	
+	@Override
+	public double getCalories() {
+		return calories;
+	}
+	
+	public String getGoalWeightLoss() {
+		return goalWeightLoss;
+	}
+	
+	public double getCurrentWeight() {
+		return currentWeight;
+	}
+	
+	public void setGoalWeightLoss(String goalWeight) {
+		this.goalWeightLoss = goalWeight;
+	}
+	
+	public void setCurrentWeight(double currentWeight) {
+		this.currentWeight = currentWeight;
+	}
+	
+	public void setWeightGoal(double goalWeight) {
+		this.goalWeight = goalWeight;
+	}
+	
+	public double getWeightGoal() {
+		return goalWeight;
+	}
+	
+	public int getWeightDifference() {
+		return weightDifference;
+	}
+	
+	
+
 	public Goals(String goalValue, double currentValue) {
-		goalWeight = goalValue;
+		goalWeightLoss = goalValue;
 		currentWeight = currentValue;
 	}
 	
 	public Goals() {
 		
 	}
+	
 	
 	public double CurrentWeightKg() {
 		double currentWeightKg = currentWeight*0.453592;
@@ -23,13 +74,31 @@ public class Goals {
 		double randomNumber = (Math.random() * (0.02 - 0.01)) + 0.01;
 		return randomNumber;
 	}
+	
 
-	public String getGoalWeight() {
-		return goalWeight;
-	}
 	
-	public double getCurrentWeight() {
-		return currentWeight;
-	}
+	public XYChart.Series timeToGoal() {
+		XYChart.Series series = new XYChart.Series();
 	
+		series.setName("Goal weight per week");
+
+		int weightDifference = Math.abs((int) (currentWeight - goalWeight));
+		this.weightDifference = weightDifference;
+		int weightByWeek = (int) currentWeight;
+		
+		// using wrong math for this method
+		if (goalWeightLoss == "Weight loss of 1 Ibs (0.5kg) per week") {
+			for (int week = 0; week <= weightDifference; week++) {
+				series.getData().add(new XYChart.Data(String.format("Week %o", week), weightByWeek));
+				weightByWeek--;
+			}
+		} else if (goalWeightLoss == "Weight gain of 1 Ibs (0.5kg) per week") {
+			for (int week = 0; week <= weightDifference; week++) {
+				series.getData().add(new XYChart.Data(String.format("Week %o", week), weightByWeek));
+				weightByWeek++;
+			}
+		}
+		
+		return series;
+	}
 }
